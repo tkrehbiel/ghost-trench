a2a06a64ea40f8979c96b270efd1b6ba424f1576
Thomas Krehbiel <tkrehbiel@salesforce.com>
2022-06-24 15:48:30 -0400

    yaml tests

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-from pathlib import Path
+import yaml
 
 
-def main(pathname):
-    p = Path(pathname)
-    for f in sorted(p.glob("*.*"), key=lambda i: i.name.casefold()):
-        print(f.name)
+def load_index(pathname: str):
+    import os
+    filename = os.path.join(pathname, "_index.yaml")
+    with open(filename, "r") as file:
+        index = yaml.safe_load(file)
+    testname = os.path.join(pathname, "_index_rewrite.yaml")
+    with open(testname, "w") as file:
+        yaml.dump(index, file, sort_keys=False, encoding="utf-8", allow_unicode=True, width=999)
+    return
+
+
+def main(pathname: str):
+    from pathlib import Path
+    pathinfo = Path(pathname)
+    files = sorted(pathinfo.glob("*.mp4"), key=lambda i: i.name.casefold())
+    for file in files:
+        print(file.name)
+    return
 
 
 if __name__ == "__main__":
     import sys
-    path = "."
+
+    path = "./files"
     if len(sys.argv) > 1:
         path = sys.argv[1]
-    print(path)
+    print("path:", path)
+    load_index(path)
     main(path)
